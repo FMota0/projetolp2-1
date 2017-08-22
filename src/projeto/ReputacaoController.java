9615b30b96d51c5f4f117def9d1117251f8081c7
annegsilva <anne.silva@ccc.ufcg.edu.br>
2017-08-21 23:49:33 -0300

    documentado

--- src/projeto/ReputacaoController.java
+++ src/projeto/ReputacaoController.java
@@ -1,42 +1,49 @@
 package projeto;
 
+/**
+ * Classe controladora das atividades relacionadas a reputacao dos usuarios
+ * 
+ * @author Hugo
+ *
+ */
 public class ReputacaoController {
 	private double reputacao;
 	private CartaoFidelidade cartaofidelidade;
-	
-	public ReputacaoController(){
+
+	/**
+	 * Inicia o Controller com todos os cartoes fidelidades com o tipo "FreeRyder" e
+	 * a nota da repotucao como 0.
+	 */
+	public ReputacaoController() {
 		this.reputacao = 0.0;
 		this.cartaofidelidade = new FreeRyder();
 	}
-	
-	public void addReputacao(double reputacao, boolean temItens){
+
+	public void addReputacao(double reputacao, boolean temItens) {
 		this.reputacao += reputacao;
-		if(this.reputacao >= 0 && this.reputacao <= 100 && temItens) {
+		if (this.reputacao >= 0 && this.reputacao <= 100 && temItens) {
 			this.cartaofidelidade = new Noob();
-		}
-		else if(this.reputacao > 100 && temItens) {
+		} else if (this.reputacao > 100 && temItens) {
 			this.cartaofidelidade = new BomAmigo();
-		}
-		else if(this.reputacao < 0) {
+		} else if (this.reputacao < 0) {
 			this.cartaofidelidade = new Caloteiro();
-		}
-		else if(this.reputacao >= 0 && !temItens) {
+		} else if (this.reputacao >= 0 && !temItens) {
 			this.cartaofidelidade = new FreeRyder();
 		}
 	}
-	
-	public double getReputacao(){
+
+	public double getReputacao() {
 		return this.reputacao;
 	}
-	
-	public boolean podeEmprestar(){
+
+	public boolean podeEmprestar() {
 		return cartaofidelidade.podeEmprestar();
 	}
-	
+
 	public int periodoMaximo() {
 		return this.cartaofidelidade.periodoMaximo();
 	}
-	
+
 	public String toString() {
 		return this.cartaofidelidade.toString();
 	}
